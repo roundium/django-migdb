64f60cd92719aedc7893ba961af07c762b671c53
mohammad katoozi <k2527806@gmail.com>
2019-07-29 11:03:05 +0430

    remove add prefix and postfix and replace from js and forms.py file

--- migdb/forms.py
+++ migdb/forms.py
@@ -5,7 +5,6 @@ from django import forms
 ACTIONS = (
     ("nochange", "No Change"),
     ("rename", "Rename"),
-    ("replace", "Replace"),
     ("format", "Format"),
     ("delete", "Delete"),
     ("concat", "Concat"),
@@ -19,7 +18,6 @@ class FieldForm(forms.Form):
     m2m = forms.BooleanField(required=False)
 
     new_field_name = forms.CharField(max_length=255, required=False)
-    replace_value = forms.CharField(max_length=255, required=False)
     format_value = forms.CharField(max_length=255, required=False)
     concat_field = forms.CharField(max_length=255, required=False)
     concat_delimiter = forms.CharField(max_length=255, required=False)
--- migdb/static/migdb/js/base.js
+++ migdb/static/migdb/js/base.js
@@ -4,18 +4,6 @@ function action_changes(obj, div_id, forloop_coun){
         $("#" + div_id).html(
             'new field name: <input type="text" name="form-' + forloop_coun + '-new_field_name" required/>'
         )
-    }else if(user_action == 'replace'){
-        $("#" + div_id).html(
-            'replace value: <input type="text" name="form-' + forloop_coun + '-replace_value" required/>'
-        )
-    }else if(user_action == 'prefix'){
-        $("#" + div_id).html(
-            'prefix value: <input type="text" name="form-' + forloop_coun + '-prefix_value" required/>'
-        )
-    }else if(user_action == 'postfix'){
-        $("#" + div_id).html(
-            'postfix value: <input type="text" name="form-' + forloop_coun + '-postfix_value" required/>'
-        )
     }else if(user_action == 'format'){
         $("#" + div_id).html(
             'format value: <input type="text" name="form-' + forloop_coun + '-format_value" required/> -> {current_value} for field default value'
