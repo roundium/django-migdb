b5134b0aecc1433dbce131e471b6cdf2b0857574
mohammad katoozi <k2527806@gmail.com>
2019-07-29 13:39:18 +0430

    create select tag with options for concat choices

--- migdb/static/migdb/js/base.js
+++ migdb/static/migdb/js/base.js
@@ -1,3 +1,13 @@
+function generate_fields_select_options(){
+    contact_select_options_text = "";
+    arrayLength = fields_list.length;
+    for (var i = 0; i < arrayLength; i++) {
+        value = fields_list[i];
+        contact_select_options_text = contact_select_options_text + "<option value='" + value + "'>" + value + "</option>";
+    }
+    return contact_select_options_text
+}
+
 function action_changes(obj, div_id, forloop_coun){
     user_action = obj.value
     if(user_action == "rename"){
@@ -12,13 +22,13 @@ function action_changes(obj, div_id, forloop_coun){
         $("#" + div_id).html('')
     }else if(user_action == 'concat'){
         $("#" + div_id).html(
-            'concat field: <input type="text" name="form-' + forloop_coun + '-concat_field" required/> '+
+            'concat field: <select name="form-' + forloop_coun + '-concat_field" required>'+ generate_fields_select_options() +'</select> '+
             'concat delimiter: <input type="text" value=" " name="form-' + forloop_coun + '-concat_delimiter" required/> --> default is space'
         )
     }else if(user_action == 'concat_rename'){
         $("#" + div_id).html(
             'new field name: <input type="text" name="form-' + forloop_coun + '-new_field_name" required/> ' +
-            'concat field: <input type="text" name="form-' + forloop_coun + '-concat_field" required/> '+
+            'concat field: <select name="form-' + forloop_coun + '-concat_field" required>'+ generate_fields_select_options() +'</select> '+
             'concat delimiter: <input type="text" value=" " name="form-' + forloop_coun + '-concat_delimiter" required/> --> default is space'
         )
     }
