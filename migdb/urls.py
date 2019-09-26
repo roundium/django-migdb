6d2a7c1ade8caffc07bd59214f7320e2e456d34f
mohammad katoozi <k2527806@gmail.com>
2019-09-26 12:41:35 +0330

    fix django urls issue. fix the package.json script issues

--- migdb/urls.py
+++ migdb/urls.py
@@ -6,7 +6,6 @@ app_name = 'migdb'
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("apps/", apps_list, name="apps_list"),
-    path("apps/", apps_list, name="app_models"),
     path("models/", ModelsList.as_view(), name="models_list"),
     path("models/fields/", FieldsList.as_view(), name="fields_list"),
 ]
