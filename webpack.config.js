55e40b6f8b80239df387080f00070dfda5089515
mohammad katoozi <k2527806@gmail.com>
2019-10-03 13:11:03 +0330

    add the ant ui

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
 var path = require("path");
-var webpack = require('webpack');
-var BundleTracker = require('webpack-bundle-tracker');
+var webpack = require("webpack");
+var BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
   context: __dirname,
 
-  entry: './frontend/index',
+  entry: "./frontend/index",
 
   output: {
-      path: path.resolve('./migdb/static/migdb/js/'),
-      filename: "[name].js",
+    path: path.resolve("./migdb/static/migdb/js/"),
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
-  }
-
+    extensions: ["*", ".js", ".jsx"],
+  },
 };
