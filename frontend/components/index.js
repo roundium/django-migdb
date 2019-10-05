4dec7b97a91da6cb0abe44b76788628b863aaf14
mohammad katoozi <k2527806@gmail.com>
2019-10-05 12:07:46 +0330

    export all components in index.js

---
+++ frontend/components/index.js
@@ -0,0 +1,5 @@
+export {default as Header} from "./Header";
+export {default as Footer} from "./Footer";
+export {default as AppsList} from "./AppsList";
+export {default as ModelsList} from "./ModelsList";
+export {default as FieldsList} from "./FieldsList";
--- frontend/index.js
+++ frontend/index.js
@@ -3,36 +3,31 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "antd/dist/antd.css";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import AppsList from "./components/AppsList";
-import ModelsList from "./components/ModelsList";
-import FieldsList from "./components/FieldsList";
+import {
+  Header,
+  Footer,
+  AppsList,
+  ModelsList,
+  FieldsList,
+} from "./components";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      base_url: window.location.pathname,
-    };
-  }
-  render() {
-    return (
-      <Router basename={this.state.base_url}>
-        <div>
-          <Header />
-          <Route exact path="/" component={AppsList} />
-          <Route exact path="/:app_name/models/" component={ModelsList} />
-          <Route
-            exact
-            path="/:app_name/:model_name/fields/"
-            component={FieldsList}
-          />
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  const base_url = window.location.pathname;
+  return (
+    <Router basename={base_url}>
+      <div>
+        <Header />
+        <Route exact path="/" component={AppsList} />
+        <Route exact path="/:app_name/models/" component={ModelsList} />
+        <Route
+          exact
+          path="/:app_name/:model_name/fields/"
+          component={FieldsList}
+        />
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("react"));
