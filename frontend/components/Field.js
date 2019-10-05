e39fb149a5596fbe2cfc77b65d8c880561e5b48e
mohammad katoozi <k2527806@gmail.com>
2019-10-05 11:57:37 +0330

    upgrade some components to use ant package

--- frontend/components/Field.js
+++ frontend/components/Field.js
@@ -1,23 +1,13 @@
 import React from "react";
-import Accordion from "react-bootstrap/Accordion";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { Collapse } from 'antd';
+
+const { Panel } = Collapse;
 
 const Field = ({field, id}) => {
   return (
-    <Card>
-      <Card.Header role="tab" id="headingTwo1">
-        <Accordion.Toggle as={Button} variant="link" eventKey={id}>
-          <h6 className="mb-0">
-            {field.name}
-            <i className="fas fa-angle-down rotate-icon"></i>
-          </h6>
-        </Accordion.Toggle>
-      </Card.Header>
-      <Accordion.Collapse eventKey={id}>
-        <Card.Body>{field.name}</Card.Body>
-      </Accordion.Collapse>
-    </Card>
+    <Panel header={field.name} key={id}>
+      <p>{field.name}</p>
+    </Panel>
   );
 };
 
--- frontend/components/FieldsList.js
+++ frontend/components/FieldsList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Field from "./Field";
-import Accordion from "react-bootstrap/Accordion";
+import { Collapse } from "antd";
+
+const { Panel } = Collapse;
 
 export default class FieldsList extends React.Component {
   constructor(props) {
@@ -42,64 +44,6 @@ export default class FieldsList extends React.Component {
     let fields = this.state.fields.map((field, i) => (
       <Field field={field} id={i} key={i} />
     ));
-    return (
-      <main>
-        <div className="container-fluid">
-          <section>
-            <div className="row">
-              <div className="col-lg-12 col-md-12 mb-lg-0 mb-4"></div>
-            </div>
-            <div className="row">
-              <div className="col-lg-12 col-md-12 mb-md-0 mb-4">
-                <div className="card">
-                  <div className="card-header white-text primary-color">
-                    Migrate {this.state.model_name} Model
-                  </div>
-                  <div className="card-body text-left px-4 mb-3">
-                    <form action="" method="POST">
-                      <div className="row">
-                        <div className="col-md-4 px-3">
-                          <div className="md-form my-0">
-                            <input
-                              type="text"
-                              id="model-name"
-                              name="new_model_name"
-                              className="form-control"
-                              placeholder="New Model Name"
-                              defaultValue={this.state.model_name}
-                              onChange={this.save_new_model_name}
-                            />
-                          </div>
-                        </div>
-                      </div>
-                      <hr className="hr-light" />
-                      <div
-                        className="accordion md-accordion"
-                        id="accordionEx1"
-                        role="tablist"
-                        aria-multiselectable="true"
-                      >
-                        <Accordion>{fields}</Accordion>
-                      </div>
-                      <hr className="hr-light my-3" />
-                      <div className="row">
-                        <div className="col-md-4">
-                          <button
-                            onClick={this.dump_data_btn}
-                            className="btn btn-primary mx-0"
-                          >
-                            Dump Data
-                          </button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-      </main>
-    );
+    return <Collapse accordion>{fields}</Collapse>;
   }
 }
